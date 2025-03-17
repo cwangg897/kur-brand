9762016bd6347b710e1ac3a24bda287cbfbd7869
cwangg897 <dhkdrb897@gmail.com>
2025-03-17 21:39:57 +0900

    init commit

---
+++ src/main/java/com/kurbrand/KurBrandApplication.java
@@ -0,0 +1,13 @@
+package com.kurbrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurBrandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurBrandApplication.class, args);
+    }
+
+}
